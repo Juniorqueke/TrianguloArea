1782b76af73fc4c40882146df3563bc8e3ac618e
Rogerio Andre Queke Junior <56850540+Juniorqueke@users.noreply.github.com>
2020-05-07 22:43:24 -0300

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,19 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class AreaTriangulo {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite o valor da base");
+		double base = entrada.nextDouble();
+		System.out.println("Digite o valor da altura");
+		double altura = entrada.nextDouble();
+		double area = base * altura / 2;
+		System.out.println(area);
+		entrada.close();
+	}
+
+}
